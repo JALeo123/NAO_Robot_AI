2be6f3acf4203696d705823bab111cb6b880c306
David Schendt <david.schendt@gmail.com>
2021-05-04 17:40:45 -0600

    Add evaluation and more wrapper functions to emotion model

--- EmotionModel.py
+++ EmotionModel.py
@@ -2,14 +2,17 @@
 # that my GPU isn't CUDA compatible...so I don't get ANY warning now
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 from tensorflow import keras
 from tensorflow.keras.preprocessing import image
 import numpy as np
+import glob
 import cv2
 
 class Emotion:
     def __init__(self):
         self.emotion_model = keras.models.load_model('emotion_model')
+        self.last_result = None
         self.ANGRY = 0
         self.DISGUST = 1
         self.FEAR = 2
@@ -33,21 +36,87 @@ class Emotion:
         classes = np.argmax(prediction, axis=1)
         #print(classes[0])
         # Return label index of prediction
-        return classes[0]
+        self.last_result = classes[0]
+        return self.last_result
 
-    def label_to_string(self, label):
+    def result_string_raw(self):
         switcher = {
-            self.ANGRY: "Angry",
-            self.DISGUST: "Disgust",
-            self.FEAR: "Fear",
-            self.HAPPY: "Happy",
-            self.NEUTRAL: "Neutral",
-            self.SAD: "Sad",
-            self.SURPRISE: "Surprise",
+            self.ANGRY:     "Angry",
+            self.DISGUST:   "Disgust",
+            self.FEAR:      "Fear",
+            self.HAPPY:     "Happy",
+            self.NEUTRAL:   "Neutral",
+            self.SAD:       "Sad",
+            self.SURPRISE:  "Surprise",
+            None:           "Error",
         }
-        return switcher.get(label)
+        return switcher.get(self.last_result)
 
-emotion_class = Emotion()
-img = cv2.imread(r'C:\Users\david\Desktop\FER\train\angry\Training_3908.jpg')
-label = emotion_class.evaluate(img)
-print(emotion_class.label_to_string(label))
+    def result_string_reduced(self):
+        switcher = {
+            self.HAPPY:     "Positive",
+
+            self.NEUTRAL:   "Neutral",
+
+            self.ANGRY:     "Negative",
+            self.DISGUST:   "Negative",
+            self.FEAR:      "Negative",
+            self.SAD:       "Negative",
+            self.SURPRISE:  "Negative",
+
+            None:           "Error",
+        }
+        return switcher.get(self.last_result)
+
+    def result_int_reduced(self):
+        switcher = {
+            self.HAPPY:     1,
+
+            self.NEUTRAL:   0,
+
+            self.ANGRY:     (-1),
+            self.DISGUST:   (-1),
+            self.FEAR:      (-1),
+            self.SAD:       (-1),
+            self.SURPRISE:  (-1),
+
+            None:           0,
+        }
+        return switcher.get(self.last_result)
+
+# Functions not associated with class
+
+
+def example():
+    emotion_class = Emotion()
+    img = cv2.imread(r'C:\Users\david\Desktop\FER\train\angry\Training_3908.jpg')
+    emotion_class.evaluate(img)
+    print(emotion_class.result_string_raw())
+    print(emotion_class.result_string_reduced())
+    return
+
+
+def evaluateDirectory():
+    positives = 0
+    neutrals = 0
+    negatives = 0
+
+    emotion_class = Emotion()
+    files = glob.glob(r"C:\Users\david\Desktop\FER\test\surprise\*.jpg")
+    for file in files:
+        # Read the image
+        image = cv2.imread(file)
+        emotion_class.evaluate(image)
+        if emotion_class.result_int_reduced() == 0:
+            neutrals += 1
+        elif emotion_class.result_int_reduced() == 1:
+            positives += 1
+        elif emotion_class.result_int_reduced() == -1:
+            negatives += 1
+    print("Positives: ", positives)
+    print("Neutrals: ", neutrals)
+    print("Negatives: ", negatives)
+    return
+
+
+#evaluateDirectory()
